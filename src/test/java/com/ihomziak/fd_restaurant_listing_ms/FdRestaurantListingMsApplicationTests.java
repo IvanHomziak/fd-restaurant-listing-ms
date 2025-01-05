ad27359a213c063248cc987603ead4431cc94afd
ivanhomziak <ivan.homziak@gmail.com>
2025-01-05 20:47:26 +0100

    Initial commit

---
+++ src/test/java/com/ihomziak/fd_restaurant_listing_ms/FdRestaurantListingMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ihomziak.fd_restaurant_listing_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FdRestaurantListingMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
